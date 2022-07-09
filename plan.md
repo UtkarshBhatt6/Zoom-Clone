6490fc79ca77685540dd844134610549d4d9a3da
Utkarsh <utkarshbhatt@kgpian.iitkgp.ac.in>
2022-07-09 14:44:55 +0530

    first commit

---
+++ plan.md
@@ -0,0 +1,2 @@
+# plan of action
+-Initialize
---
+++ server.js
@@ -0,0 +1,30 @@
+const express= require('express')
+const app=express()
+const http=require('http');
+const { ExpressPeerServer } = require('peer');
+const server=http.createServer(app);
+const {v4:uuidv4}=require('uuid');
+const io=require('socket.io')(server)
+const peerServer=ExpressPeerServer(server,{
+    debug: true
+})
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use('/peerjs',peerServer);
+app.get('/',(req,res)=>{
+    res.redirect(`/${uuidv4()}`);
+})
+app.get('/:room',(req,res)=>{
+    res.render('room',{roomId: req.params.room})
+})
+io.on('connection',socket=>{
+    socket.on('join-room',(roomId,userId)=>{
+        socket.join(roomId)
+        socket.broadcast.to(roomId).emit('user-connected',userId);
+        socket.on('message', message =>{
+            io.to(roomId).emit('createMessage',message);
+        })
+    })
+})
+
+server.listen(8080)
